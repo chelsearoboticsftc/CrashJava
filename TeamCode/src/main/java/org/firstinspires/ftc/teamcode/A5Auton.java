effbaf13303aaf8df18c5e6ea590ba6ad0147162
rbrown518 <rbrown518@icloud.com>
2024-10-19 11:43:02 -0400

    Update A5Auton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/A5Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/A5Auton.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.util;
+package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
